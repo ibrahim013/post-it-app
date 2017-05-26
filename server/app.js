e23f100b3d2a4edb447afa4ef936350ea3a0caaa
ibrahim013 <waleibrahim13@gmail.com>
2017-05-26 14:23:17 +0200

    Files Restructured

--- server/app.js
+++ server/app.js
@@ -1,34 +1,28 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const index = require('./routes/user');
 
+// BASE SETUP==========================================
+import express from 'express';
+import bodyParser from 'body-parser';
+import index from './routes/user';
+
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POSTS');
-	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, \
-		content-type, Authorization');
-	next();
-});
-
-app.use(morgan('dev'));  // log all requests to the console
-
-app.get('/', function(req, res) {
-	res.send('welcome to the home page!');
-	console.log(ref);
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POSTS');
+  next();
 });
 
 
+app.get('/', (req, res) => {
+  res.send('welcome to the home page!');
+});
 
 
-// API MIDDLEWARE ============================================================
-
-
+// API LISTNER ============================================================
 app.use('/api', index);
 app.listen(8000);
 console.log('Runing on port 8000');
+
