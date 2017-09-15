1492333cefd558e0e6a6dd857e31439259f06203
Ibrahim Abdulazeez <ibrahim.abdulazeez@andela.com>
2017-09-16 00:33:21 +0100

    Update GetGroupList.jsx

--- client/component/GetGroupList.jsx
+++ client/component/GetGroupList.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import Group from './groups';
+import Group from './Groups';
 import { getGroups } from '../actions/GetGroupsAction';
 
 
