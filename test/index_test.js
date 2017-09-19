0f98c7c2b6b6e682111e8c0fcbce83b4c0cf7878
ibrahim013 <walaibrahim13@gmail.com>
2017-09-19 03:31:33 +0100

    file refactored

--- test/index_test.js
+++ test/index_test.js
@@ -1,44 +1,42 @@
-'use strict';
 
-let request = require('supertest')
-let server = 'parseInt(process.env.PORT, 10) || 3000';
-let should = require("should");
 
-describe('/GET', () => {
-	it('should respond with a 404',(done)=>{
-		request(server)
-		.get('/dummy/path')
-		.expect(404)
-			done();
-		})
-	})
+const request = require('supertest');
+
+const server = 'parseInt(process.env.PORT, 10) || 3000';
+const should = require('should');
 
 describe('/GET', () => {
-	it('should respond with index path',(done)=>{
-		request(server)
-		.get('/')
-		.expect(200)
+  it('should respond with a 404', (done) => {
+    request(server)
+      .get('/dummy/path')
+      .expect(404);
+    done();
+  });
+});
+
+describe('/GET', () => {
+  it('should respond with index path', (done) => {
+    request(server)
+      .get('/')
+      .expect(200);
 		 done();
-			
-		})
-	})
+  });
+});
 
 describe('/POST', () => {
-	it('should respond with signin route',(done)=>{
-		request(server)
-		.post('/user/signin')
-		.expect(200)
+  it('should respond with signin route', (done) => {
+    request(server)
+      .post('/user/signin')
+      .expect(200);
 		 done();
-			
-		})
-	})
+  });
+});
 describe('/GET', () => {
-	it('should respond with dashboard',(done)=>{
-		request(server)
-		.get('/dashboard')
-		.expect(200)
+  it('should respond with dashboard', (done) => {
+    request(server)
+      .get('/dashboard')
+      .expect(200);
 		 done();
-			
-		})
-	})
-	
+  });
+});
+
