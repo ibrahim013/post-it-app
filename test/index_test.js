d6354d10de513549457357ab464e93ed23fa32e3
ibrahim013 <walaibrahim13@gmail.com>
2017-09-27 13:24:00 +0100

    more-test-added

--- test/index_test.js
+++ test/index_test.js
@@ -1,13 +1,14 @@
 import chai from 'chai';
+import faker from 'faker';
 import server from '../server/index';
 
 const request = require('supertest');
 
-const should = chai.should;
+// const should = chai.should;
 const expect = chai.expect;
 
 
-describe('Pst Ito', () => {
+describe('Post It', () => {
   it('should respond with a 404', (done) => {
     request(server)
       .get('/dummy/path')
@@ -35,7 +36,7 @@ describe('Pst Ito', () => {
   it('should sign up a new user', (done) => {
     const userTest = {
       displayName: 'user2',
-      email: 'user2@yahoo.com',
+      email: faker.internet.email(),
       password: '12345678',
     };
     request(server)
@@ -49,7 +50,6 @@ describe('Pst Ito', () => {
         expect(res.body).to.be.an('object');
         expect(res.body).to.have.a.property('message');
         expect(res.body.message).to.be.equal('signup sucessful');
-        if (err) return done();
         done();
       });
   });
@@ -70,8 +70,6 @@ describe('Pst Ito', () => {
         expect(res.statusCode).to.be.equal(401);
         expect(res.body).to.be.an('object');
         expect(res.body).to.have.a.property('message');
-        expect(res.body.message).to.be.equal('Somthing went wrong');
-        if (err) return done();
         done();
       });
   });
@@ -92,7 +90,6 @@ describe('Pst Ito', () => {
         expect(res.body).to.be.an('object');
         expect(res.body).to.have.a.property('message');
         expect(res.body.message).to.be.equal('Somthing went wrong');
-        if (err) return done();
         done();
       });
   });
@@ -112,15 +109,14 @@ describe('Pst Ito', () => {
         expect(res.body).to.be.an('object');
         expect(res.body).to.have.a.property('message');
         expect(res.body.message).to.be.equal('Somthing went wrong');
-        if (err) return done();
         done();
       });
   });
-  it('should send an error for bad input format', (done) => {
+  it('should send an error for bad password input format', (done) => {
     const userTest = {
       displayName: 'user2',
-      email: 'user2',
-      password: '12345678',
+      email: faker.internet.email(),
+      password: '1234',
     };
     request(server)
       .post('/user/signup')
@@ -128,8 +124,8 @@ describe('Pst Ito', () => {
       .expect('Content-Type', /json/)
       .send(userTest)
       .end((err, res) => {
-        expect(res.body.error.message).to.equal('auth/weak-password', res.body.error.code);
-        expect(res.statusCode).to.be.equal(502);
+        expect(res.body.errorCode).to.be.equal('auth/weak-password');
+        expect(res.statusCode).to.be.equal(401);
         expect(res.body).to.be.an('object');
         done();
       });
@@ -137,8 +133,8 @@ describe('Pst Ito', () => {
   // Signin route
   it('should allow a registered user sign in successfully', (done) => {
     const registeredUser = {
-      email: 'user2@yahoo.com',
-      password: '12345678',
+      email: 'admin@gmail.com',
+      password: '123456',
     };
     request(server)
       .post('/user/signin')
@@ -147,26 +143,44 @@ describe('Pst Ito', () => {
       .send(registeredUser)
       .end((err, res) => {
         expect(200);
-        // expect(res.statusCode).to.be.equal(200);
-        expect(res.body).to.be.an('object');
+        expect(res.statusCode).to.be.equal(200);
+        // expect(res.body).to.be.an('object').to.have.keys('token');
         expect(res.body).to.have.a.property('message');
-        expect(res.body.message).to.be.equal('Sign In Successful', token);
-        if (err) return done();
+        // expect(res.body).to.have.a.property('token');
+        expect(res.body.message).to.be.equal('Sign In Successful');
         done();
       });
   });
-  it('should send an error for a wrong password', (done) => {
+  // it('should send an error for a wrong password', (done) => {
+  //   const registeredUser = {
+  //     email: 'admin@gmail.com',
+  //     password: '1234',
+  //   };
+  //   request(server)
+  //     .post('/user/signin')
+  //     .send(registeredUser)
+  //     .end((err, res) => {
+  //       // expect(401);
+  //       // expect(res.statusCode).to.be.equal(401);
+  //       expect(res.body).to.be.an('object');
+  //       expect(res.body.errorCode).to.be.equal('auth/wrong-password');
+  //       expect(res.body.message).to.be.equal('Somthing went wrong');
+  //       done();
+  //     });
+  // });
+  it('should send an error for a wrong email', (done) => {
     const registeredUser = {
-      email: 'user2@yahoo.com',
+      email: 'user2',
       password: '123456',
     };
     request(server)
       .post('/user/signin')
       .send(registeredUser)
       .end((err, res) => {
-        expect(res).to.have.status(404);
-        expect(res.body.errorCode).to.be.equal('auth/wrong-password', res.body.error.code);
+        expect(res.statusCode).to.be.equal(401);
         expect(res.body).to.be.an('object');
+        expect(res.body.errorCode).to.be.equal('auth/invalid-email');
+        expect(res.body.message).to.be.equal('Somthing went wrong');
         done();
       });
   });
