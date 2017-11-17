503638efea4cd2b83d29d6f9a0d0bc61d253b9ac
ibrahim013 <walaibrahim13@gmail.com>
2017-11-17 10:38:27 +0100

    add user to geoup fix

--- server/helpers/Utilities.js
+++ server/helpers/Utilities.js
@@ -5,12 +5,11 @@
  * @function capitalizeFirstLetter
  * @param {String} character
  *
- * @return {Object} a string in lowercase and the First letter in Capital
+ * @return {String} a string in lowercase and the First letter in Capital
  *
  */
- const capitalizeFirstLetter = (character) => {
-   const string = character.toLowerCase();
-   return string.charAt(0).toUpperCase() + string.slice(1);
- };
+ const capitalizeFirstLetter = character =>
+ character.charAt(0).toUpperCase() + character.slice(1);
+
 
  export default capitalizeFirstLetter;
