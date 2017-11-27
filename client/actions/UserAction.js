64baf818cc82b49c4a9950aa0358cd8a7c22a60d
ibrahim013 <walaibrahim13@gmail.com>
2017-11-27 19:43:31 +0100

    local strorage fix

--- client/actions/UserAction.js
+++ client/actions/UserAction.js
@@ -96,6 +96,7 @@ export function signOut() {
         });
         localStorage.removeItem('user');
         localStorage.removeItem('GoogleLogin');
+        localStorage.removeItem('token');
       })
       .catch((error) => {
         if (error) {
